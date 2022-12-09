2a37fd4400d029135776ec60e4a34eb44f7a0745
muqeetmughal <muqeetmughal786@gmail.com>
2022-12-09 22:24:19 +0500

    Active link and blog banner fix

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views as blog_views
 urlpatterns = [
-    path("", blog_views.index),
+    path("", blog_views.index, name="blog_home"),
     path("<slug>", blog_views.blog_detail, name="blog_detail"),
 
     
--- web/urls.py
+++ web/urls.py
@@ -3,14 +3,14 @@ from . import views as web_views
 
 
 urlpatterns = [
-    path("", web_views.home),
-    path("about-us", web_views.about),
-    path("services", web_views.services),
+    path("", web_views.home, name="home"),
+    path("about-us", web_views.about, name="about"),
+    path("services", web_views.services, name="services"),
     path("services/<slug>", web_views.service_detail),
     path("portfolio/<slug>", web_views.work_detail, name="work_detail"),
-    path("portfolio", web_views.works),
-    path("contact", web_views.contact),
-    path("careers", web_views.careers),
+    path("portfolio", web_views.works, name="work"),
+    path("contact", web_views.contact, name="contact"),
+    path("careers", web_views.careers, name="careers"),
     path("subscribe", web_views.subscribe),
 
 ]
